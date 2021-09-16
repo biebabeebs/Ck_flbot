436690d932488fe754aacfa44c6a5e14a669c3a3
biebabeebs <89168968+biebabeebs@users.noreply.github.com>
2021-09-16 13:02:52 +0530

    Delete translation.py

--- bot/translation.py
+++
@@ -1,14 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
-
-class Translation(object):
-    
-    START_TEXT = """<b>@Dustinreq..!    
-    
-    FORCE_SUB_TEXT = """🍎 <u><b>READ THIS INSTRUCTION</b></u> 🍎
-
-✪ ഫയലുകൾ ലഭിക്കുന്നതിനായി  നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം
-"ശേഷം refresh button അമർത്തുക"
-
-✪ <b>You Need To Join Our Channel and Press Refresh Button to get the File.</b>"""
