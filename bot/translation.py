be9cf64a680e00a4c705c1a7b0411f1abfdfdd65
biebabeebs <89168968+biebabeebs@users.noreply.github.com>
2021-09-16 22:33:23 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,6 +4,4 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>@Dustinreq..!    
-    
-    FORCE_SUB_TEXT = """"You Need To Join Our Channel and Press Refresh Button to get the file\n🍎ഫയലുകൾ ലഭിക്കുന്നതിനായി നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം\n🍎 ശേഷം refresh button അമർത്തുക",.</b>"""
+    START_TEXT = """<b>@Dustinreq..!
