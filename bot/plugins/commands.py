64ed7850ad8ad124615bb150ce547f0b970381c3
biebabeebs <89168968+biebabeebs@users.noreply.github.com>
2021-09-17 10:17:41 +0530

    Add files via upload

---
+++ bot/plugins/commands.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+from pyrogram import filters, Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import UserNotParticipant
+from bot import Translation, LOGGER # pylint: disable=import-error
+from bot.database import Database # pylint: disable=import-error
+
+db = Database()
+
+@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+async def start(bot, update):
+    
+    try:
+        file_uid = update.command[1]
+    except IndexError:
+        file_uid = False
+    
+    if file_uid:
+        try:
+            member = await bot.get_chat_member("CK_Anime", update.chat.id)
+            if member.status == "kicked":
+                await bot.send_message(
+                       chat_id=update.chat.id,
+                       text="**You're  B A N N E D**",
+                       parse_mode='markdown',
+                       reply_to_message_id=update.message_id
+                       )
+                return
+
+        except UserNotParticipant:
+            me = await bot.get_me()
+            await bot.send_message(
+                    chat_id=update.chat.id,
+                    text=Translation.FORCE_SUB_TEXT,
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Channel",url="https://t.me/CK_Anime")],
+                                                       [InlineKeyboardButton(text="Refresh", url=f"https://t.me/{me.username}?start={file_uid}")]]),
+                    reply_to_message_id=update.message_id
+                    )
+            return
+
+        except Exception:
+            print('Unable to Verify')
+            await bot.send_message(
+                     chat_id=update.chat.id,
+                     text="```Something Went Wrong```",
+                     parse_mode='markdown',
+                     reply_to_message_id=update.message_id
+                     )
+            return
+
+        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
+        
+        if (file_id or file_type) == None:
+            return
+        
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        try:
+            await update.reply_cached_media(
+                file_id,
+                quote=True,
+                caption=caption,
+                parse_mode="html"
+            )
+        except Exception as e:
+            await update.reply_text(f"<b>Error:</b>\n<code>{e}</code>", True, parse_mode="html")
+            LOGGER(__name__).error(e)
+        return
+
+    buttons = [[
+        InlineKeyboardButton('Close 🔐', callback_data="close")
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
+
+#@Client.on_message(filters.command(["help"]) & filters.private, group=1)
+async def help(bot, update):
+    buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('About 🚩', callback_data='about')
+    ],[
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_TEXT,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
+
+#@Client.on_message(filters.command(["about"]) & filters.private, group=1)
+async def about(bot, update):
+    
+    buttons = [[
+        InlineKeyboardButton('Home ⚡', callback_data='start'),
+        InlineKeyboardButton('Close 🔐', callback_data='close')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_TEXT,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
