d5c54e03c767bc8d97bfe6d6f9afe48e1832ed79
biebabeebs <89168968+biebabeebs@users.noreply.github.com>
2021-11-11 20:30:25 +0530

    Update commands (1).py

--- bot/plugins/commands (1).py
+++ bot/plugins/commands (1).py
@@ -20,7 +20,7 @@ async def start(bot, update):
     
     if file_uid:
         try:
-            member = await bot.get_chat_member("DustinUploads", update.chat.id)
+            member = await bot.get_chat_member("Dustinreq", update.chat.id)
             if member.status == "kicked":
                 await bot.send_message(
                        chat_id=update.chat.id,
@@ -35,7 +35,7 @@ async def start(bot, update):
             await bot.send_message(
                     chat_id=update.chat.id,
                     text="You Need To Join Our Channel and Press Refresh Button to get the file\n🍎ഫയലുകൾ ലഭിക്കുന്നതിനായി  നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം\n🍎 ശേഷം refresh button അമർത്തുക",
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Channel",url="https://t.me/DustinUploads")],
+                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Channel",url="https://t.me/Dustinreq")],
                                                        [InlineKeyboardButton(text="Refresh", url=f"https://t.me/{me.username}?start={file_uid}")]]),
                     reply_to_message_id=update.message_id
                     )
