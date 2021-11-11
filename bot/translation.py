c8fac6533b273b850a6a13c108adb0a0dc9cad5a
biebabeebs <89168968+biebabeebs@users.noreply.github.com>
2021-11-11 20:19:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,5 +5,3 @@
 class Translation(object):
     
     START_TEXT = """<b>@cinema_kotaka..!</b>"""
-
-FORCE_SUB_TEXT = "You Need To Join Our Channel and Press Refresh Button to get the file\n🍎ഫയലുകൾ ലഭിക്കുന്നതിനായി  നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ join ചെയ്യണം\n🍎 ശേഷം refresh button അമർത്തുക",
